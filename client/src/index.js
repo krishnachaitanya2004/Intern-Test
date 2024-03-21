e6ee6f2934d2e3071bbe8a7732c17b357e8c1262
krishnachaitanya2004 <chaitu.gorle@gmail.com>
2024-03-21 17:26:00 +0530

    Test

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
 import { BrowserRouter, Routes,  Route } from 'react-router-dom';
 
-import Model1 from './Model1.js';
 import Model2 from './Model2.js';
 import Model3 from './Model3.js';
 
@@ -14,9 +13,8 @@ root.render(
   <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/models/1/try" element={<Model1 />}  />
-        <Route path="/models/2/try" element={<Model2 />}  />
-        <Route path="/models/3/try" element={<Model3 />}  />
+        <Route path="/models/1/try" element={<Model2 />}  />
+        <Route path="/models/2/try" element={<Model3 />}  />
     </Routes>
     </BrowserRouter>
 
--- server/index.js
+++ server/index.js
@@ -2,34 +2,32 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { pipeline } from '@xenova/transformers';
-import * as toxicity from '@tensorflow-models/toxicity';
 
-dotenv.config();
 const app = express();
-const port =  process.env.PORT || 11000; 
+const port = 11000; 
 
 
 app.use(cors());
 app.use(bodyParser.json());
 
-let model_loaded_1 = false;
-let model_1;
-toxicity.load(0.9).then((x) => { model_loaded_1 = true; model_1 = x; });
+// let model_loaded_1 = false;
+// let model_1;
+// toxicity.load(0.9).then((x) => { model_loaded_1 = true; model_1 = x; });
 
 app.get('/', (req, res) => {
     return res.json([
+        // {
+        //     "id":1,
+        //     "name": "Toxicity Detector",
+        //     "description": "Toxicity Detector is a pre-trained model that can detect toxic comments in a conversation. It can be used to filter out toxic comments from a conversation."
+        // },
         {
             "id":1,
-            "name": "Toxicity Detector",
-            "description": "Toxicity Detector is a pre-trained model that can detect toxic comments in a conversation. It can be used to filter out toxic comments from a conversation."
-        },
-        {
-            "id":2,
             "name": "Sentiment Analyser",
             "description": "Sentiment Analysis is a pre-trained model that can detect the sentiment of a conversation. It can be used to detect the sentiment of a conversation.It is using @xenova/transformers library."
         },
         {
-            "id":3,
+            "id":2,
             "name": "Text Summarizer",
             "description": "Text Summarizer is a pre-trained model that can summarize a conversation. It can be used to summarize a conversation. It is using @xenova/transformers library."
         },
@@ -38,18 +36,18 @@ app.get('/', (req, res) => {
 );
 
 
-app.post('/toxicity', async (req, res) => {
-    while (!model_loaded_1) {};
-    const { prompt } = req.body;
-    try {
-        let predictions = await model_1.classify(prompt);
-        predictions = predictions.filter((x) => x.label == "toxicity")[0].results[0].probabilities[1];
-        return res.json({ output: predictions });
-    } catch (error) {
-        console.error('Error loading model:', error);
-        return res.status(500);
-    }
-});
+// app.post('/toxicity', async (req, res) => {
+//     while (!model_loaded_1) {};
+//     const { prompt } = req.body;
+//     try {
+//         let predictions = await model_1.classify(prompt);
+//         predictions = predictions.filter((x) => x.label == "toxicity")[0].results[0].probabilities[1];
+//         return res.json({ output: predictions });
+//     } catch (error) {
+//         console.error('Error loading model:', error);
+//         return res.status(500);
+//     }
+// });
 
 let model_loaded_2 = false;
 let model_2;
